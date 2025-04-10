22068985f2064f26cd883e7e9d1c95c57aafb525
GetHappySay <jaredchipman@gmail.com>
2025-04-09 23:27:50 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
-// db.js
-
 module.exports = {
   saveFeedback: async (feedback) => {
     console.log("Mock saving feedback:", feedback);
     return { id: "mock-feedback-id", ...feedback };
   },
-
   updateFeedbackRouted: async (feedbackId) => {
     console.log("Mock update for feedback ID:", feedbackId);
+  },
+  getLastPrompt: async (phoneNumber, locationId) => {
+    console.log("Mock getLastPrompt for", phoneNumber, locationId);
+    return null; // simulate no prior prompt
   }
 };
